51fe34fe1382becce4196b02f8194175977efeaf
Liza Dakhno <lizadakhno@gmail.com>
2023-04-07 19:54:07 +0200

    task 2

---
+++ Lab4/Dakhno.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Lab4
+{
+    public partial class Dakhno_1
+    {
+        public static void Change(ref int[] array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i%2==0)
+                {
+                    count++;
+                }
+            }
+            int[] newArray = new int[count];
+            int j = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i%2==0)
+                {
+                    newArray[j] = array[i];
+                    j++;
+                }
+            }
+            array = newArray;
+        }
+    }
+    public partial class Dakhno_2
+    {
+        public static void Change(ref int rows, int col, ref int[][] array)
+        {
+            Console.WriteLine("Введіть діапазон який потрібно видалити:");
+            string[] range = Console.ReadLine().Trim().Split();
+            int K1 = Convert.ToInt32(range[0]);
+            int K2 = Convert.ToInt32(range[1]);
+            int[][] newArray = new int[rows-Math.Abs(K1-K2)+1][];
+            int count = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                if (i<K1-1 || i>K2-1)
+                {
+                    newArray[count] = new int[col];
+                    for (int k = 0; k < col; k++)
+                    {
+                        newArray[count][k] = array[i][k];
+                    }
+                    count++;
+                }
+            }
+            rows -= Math.Abs(K1 - K2)+1;
+            array = newArray;
+        }
+    }
+}
--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Lab4
 {
@@ -17,6 +18,7 @@ namespace Lab4
                     {
                             Console.Write(array[i]+ " ");
                     }
+                    Console.WriteLine();
                     break;
                 case 2:
                     Console.Write("Введіть масив:");
@@ -44,7 +46,7 @@ namespace Lab4
             return array;
         }
         
-        static void Output(int[] array)
+        static void Output_1(int[] array)
         {
             for (int i = 0; i < array.Length; i++)
             {
@@ -64,46 +66,126 @@ namespace Lab4
                 switch (choiceOfStudent)
                 {
                     case "дахно":
-                        Dakhno.Change(ref array);
+                        Dakhno_1.Change(ref array);
+                        Console.WriteLine("Результат:");
+                        Output_1(array);
+                        Console.WriteLine();
                         break;
                     case "бондарева":
+                        Console.WriteLine("Результат:");
+                        Output_1(array);
+                        Console.WriteLine();
                         break;
                     case "глущенко":
+                        Console.WriteLine("Результат:");
+                        Output_1(array);
+                        Console.WriteLine();
                         break;
                     default:
                         Console.WriteLine("Спробуйте ще раз");
                         break;
                 }
-                Console.WriteLine("Результат:");
-                Output(array);
-                Console.WriteLine();
                 Console.WriteLine("Введіть прізвище студента, варіант якого хочете виконати. Для завершення виконання варіанту програми введіть 0. ");
                 choiceOfStudent = Console.ReadLine().ToLower();
-                Console.WriteLine("Бажаєте створити новий масив?(Так/Ні)");
-                string answer = Console.ReadLine().ToLower();
-                if (answer == "так")
+                if (choiceOfStudent == "дахно" || choiceOfStudent == "бондарева" || choiceOfStudent == "глущенко")
                 {
-                    Console.Write("Введіть к-сть елементів: ");
-                    countOfElements = Convert.ToInt32(Console.ReadLine());
-                    array = new int[countOfElements];
-                    CreateArray_1(countOfElements, ref  array);
+                    Console.WriteLine("Бажаєте створити новий масив?(Так/Ні)");
+                    string answer = Console.ReadLine().ToLower();
+                    if (answer == "так")
+                    {
+                        Console.Write("Введіть к-сть елементів: ");
+                        countOfElements = Convert.ToInt32(Console.ReadLine());
+                        array = new int[countOfElements];
+                        CreateArray_1(countOfElements, ref  array);
+                    }
                 }
             } while (choiceOfStudent!="0");
         }
-        
+        public static void CreateArray_2(int rows, int col, ref int[][] array)
+        {
+            Console.Write("Введіть 1, щоб заповнити масив рандомно, або 2, щоб заповнити його самостійно: ");
+            int choiceOfInput = Convert.ToInt32(Console.ReadLine());
+            switch (choiceOfInput)
+            {
+                case 1:
+                    InputRandom_2(rows, col, ref array);
+                    Console.WriteLine("Початковий масив: ");
+                    for (int i = 0; i < rows; i++)
+                    {
+                        for (int j = 0; j < col; j++)
+                        {
+                            Console.Write(array[i][j] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Введіть масив:");
+                    Input_2(rows, col, ref array);
+                    break;
+            }
+        }
+        static void InputRandom_2(int rows, int col, ref int[][] array)
+        {
+            Random random = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                array[i] = new int[col];
+                for (int j = 0; j < col; j++)
+                {
+                    array[i][j] = random.Next(-100, 100);
+                }
+            }
+        }
+        static void Input_2(int rows, int col, ref int[][] array)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                string[] rowOfArray = Console.ReadLine().Trim().Split();
+                array[i] = new int[col];
+                for (int j = 0; i < col; i++)
+                {
+                    array[i][j] = Convert.ToInt32(rowOfArray[j]);
+                }
+            }
+        }
+        static void Output_2(int rows, int col, int[][] array)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write(array[i][j]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
         public static void Block_2()
         {
             Console.WriteLine("Введіть прізвище студента, варіант якого хочете виконати. Для завершення виконання варіанту програми введіть 0. ");
             string choiceOfStudent = Console.ReadLine().ToLower();
+            Console.Write("Введіть к-сть рядків та стовпчиків в масиві: ");
+            string[] size = Console.ReadLine().Trim().Split();
+            int rows = Convert.ToInt32(size[0]);
+            int col = Convert.ToInt32(size[1]);
+            int[][] array = new int[rows][];
+            CreateArray_2(rows, col, ref array);
             do
             {
                 switch (choiceOfStudent)
                 {
                     case "дахно":
+                        Dakhno_2.Change(ref rows, col, ref array);
+                        Console.WriteLine("Результат:");
+                        Output_2(rows, col, array);
                         break;
                     case "бондарева":
+                        Console.WriteLine("Результат:");
+                        Output_2(rows, col, array);
                         break;
                     case "глущенко":
+                        Console.WriteLine("Результат:");
+                        Output_2(rows, col, array);
                         break;
                     default:
                         Console.WriteLine("Спробуйте ще раз");
@@ -111,11 +193,19 @@ namespace Lab4
                 }
                 Console.WriteLine("Введіть прізвище студента, варіант якого хочете виконати. Для завершення виконання варіанту програми введіть 0. ");
                 choiceOfStudent = Console.ReadLine().ToLower();
-                Console.WriteLine("Бажаєте створити новий масив?(Так/Ні)");
-                string answer = Console.ReadLine().ToLower();
-                if (answer == "так")
+                if (choiceOfStudent == "дахно" || choiceOfStudent == "бондарева" || choiceOfStudent == "глущенко")
                 {
-                    
+                    Console.WriteLine("Бажаєте створити новий масив?(Так/Ні)");
+                    string answer = Console.ReadLine().ToLower();
+                    if (answer == "так")
+                    {
+                        Console.Write("Введіть к-сть рядків та стовпчиків в масиві: ");
+                        size = Console.ReadLine().Trim().Split();
+                        rows = Convert.ToInt32(size[0]);
+                        col = Convert.ToInt32(size[1]);
+                        array = new int[rows][];
+                        CreateArray_2(rows, col, ref  array);
+                    }
                 }
             } while (choiceOfStudent!="0");
         }
